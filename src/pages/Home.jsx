2fd57e8ecf05bf61a359c3d50f21631b4189a6bb
rayhan-sarkar27 <rayhan.sarkar370@gmail.com>
2024-05-01 01:33:41 +0600

    project for react ByteBlaze

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home</h1>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Home;
